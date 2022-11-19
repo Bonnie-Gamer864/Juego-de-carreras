00b2d90f719bfd043c5db453765694774f6f99f5
Bonnie-Gamer864 <100964239+Bonnie-Gamer864@users.noreply.github.com>
2022-11-19 11:54:11 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var canvas;
+var backgroundImage;
+var bgImg;
+var database;
+var form, player;
+var playerCount;
+var car1, car2;
+var car1_IMG, car2_IMG;
+var cars = {};
+var track;
+
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+  car1_IMG = loadImage ("./assets/car1.png");
+  car2_IMG = loadImage ("./assets/car2.png");
+  track = loadImage ("./assets/track.jpg");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+  database = firebase.database();
+  
+  game = new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw() {
+  background(backgroundImage);
+
+  if(playerCount === 2){
+    game.update(1);
+  }
+
+  if(gameState === 1){
+    game.play();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
